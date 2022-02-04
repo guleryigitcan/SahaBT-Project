7e9faff89207514c2a2ae08974610c1771579946
Ahmettozcelik <ozcelikahmett61@gmail.com>
2022-02-04 18:24:04 +0300

    Fixed book-store

--- bookstore/src/main/java/com/example/bookstore/service/BookstoreService.java
+++ bookstore/src/main/java/com/example/bookstore/service/BookstoreService.java
@@ -2,7 +2,18 @@ package com.example.bookstore.service;
 
 import org.springframework.stereotype.Service;
 
-@Service
-public class BookstoreService {
+import com.example.bookstore.dto.request.AddBookRequest;
+import com.example.bookstore.dto.request.UpdateRequest;
+import com.example.bookstore.dto.response.AddBookResponse;
+import com.example.bookstore.dto.response.DeleteBookResponse;
+import com.example.bookstore.dto.response.GetBookResponse;
+import com.example.bookstore.dto.response.UpdateBookResponse;
 
+@Service
+public interface BookstoreService {
+
+	GetBookResponse findBookByIsbn(String isbn);
+    AddBookResponse addBook(AddBookRequest request);
+    DeleteBookResponse deleteBookByIsbn(String isbn);
+    UpdateBookResponse updateBook(UpdateRequest request);
 }
